a66bfab65bb57cfeb9c52303df6545594231c87c
cappetta <thomas.cappetta@gmail.com>
2017-10-22 07:57:28 -0400

    updating kali manifest

--- terraform/SecDevOps/kali.tf
+++ terraform/SecDevOps/kali.tf
@@ -1,41 +1,31 @@
 
 resource "aws_instance" "kali" {
-  ami = "${data.aws_ami.kali}"
+  ami = "ami-10b19275"
   instance_type = "${var.instance_type}"
-  subnet_id = "${var.subnet_id}"
+  subnet_id = "${aws_subnet.us-east-2a_private.id}"
   key_name = "${var.key_name}"
   vpc_security_group_ids = ["${var.secgroup_id}"]
   user_data = "${file("${var.linux_userdata}")}"
 
   tags {
     Name = "kali"
-    Ticket = "${var.ticket}",
     Auto-Off = "True",
     Auto-Delete = "True"
   }
-  provisioner "file" {
-    source      = "../shared/linux"
-    destination = "/home/ec2-user/shared"
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      private_key = "${file("${var.key_name}")}"
-      insecure = true
-    }
-  }
+
 }
 
 
 /*
-  Public Subnet
+  private Subnet
 */
 resource "aws_subnet" "us-east-2a_private" {
   vpc_id = "${var.vpc_default}"
 
-  cidr_block = "${var.public_subnet_cidr}"
-  availability_zone = "eu-west-1a"
+  cidr_block = "${var.private_subnet_cidr}"
+  availability_zone = "us-east-2a"
 
   tags {
-    Name = "Public Subnet"
+    Name = "Autoamtion Subnet"
   }
 }
