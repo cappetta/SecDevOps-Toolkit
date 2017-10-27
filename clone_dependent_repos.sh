eee26118e48f244a4bd97d73587f591e55f61840
Thomas Cappetta <tcappetta@tenable.com>
2017-10-27 10:02:40 -0400

    adding repo clone script

---
+++ clone_dependent_repos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# there are many repo's with enormously valuable information - this downloads them to your toolbox.
+cd oscp/ && git clone https://github.com/frizb/OSCP-Survival-Guide.git && git clone https://github.com/codingo/Reconnoitre.git && git clone https://github.com/erik1o6/oscp.git && git clone https://github.com/cappetta/SecDevOps-Toolkit.wiki.git
+cd ../
