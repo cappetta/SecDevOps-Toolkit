f3aba9f27b86a121c56e3a1dd45a8c358f034a2d
cappetta <thomas.cappetta@gmail.com>
2018-09-03 02:06:59 -0400

    updating cloning of dependent repo's

--- clone_dependent_repos.sh
+++ clone_dependent_repos.sh
@@ -9,4 +9,15 @@ git clone https://github.com/darkoperator/Metasploit-Plugins.git
 cd ..
 git clone https://github.com/cappetta/SecDevOps-Toolkit.wiki.git
 git clone https://github.com/nicholasjackson/slack-bot-lex-lambda.git
+git clone https://github.com/Exploit-install/snmpwn
+git clone https://github.com/DataSploit/datasploit.git
+git clone https://github.com/offensive-security/exploit-database.git
+git clone https://github.com/scottslowe/learning-tools.git
 
+# Vulnerable AWS configurations
+git clone https://github.com/RhinoSecurityLabs/cloudgoat.git
+git clone https://github.com/Netflix/security_monkey.git
+
+git clone https://github.com/offensive-security/exploit-database
+git clone https://github.com/offensive-security/exploit-database-bin-sploits
+git clone https://github.com/offensive-security/exploit-database-papers
