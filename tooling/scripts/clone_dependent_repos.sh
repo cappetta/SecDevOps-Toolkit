1a04ed4b1386ffa023764fd8fcc9eb0af91c3d3a
cappetta <thomas.cappetta@gmail.com>
2018-12-16 21:32:19 -0500

    adding downloadgit script

--- tooling/scripts/clone_dependent_repos.sh
+++ tooling/scripts/clone_dependent_repos.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
+mkdir - /tmp/tests;
+cd /tmp/tests
 # there are many repo's with enormously valuable information - this downloads them to your toolbox.
+
+function downloadGit(){
+  echo "downloading repo: $1"
+  git clone $1
+  echo "-------------"
+
+
+}
 dir=Offensive;
 mkdir $dir; cd $dir
-git clone https://github.com/frizb/OSCP-Survival-Guide.git
-git clone https://github.com/codingo/Reconnoitre.git
-git clone https://github.com/erik1o6/oscp.git
-git clone https://github.com/scipag/vulscan.git
-git clone https://github.com/darkoperator/Metasploit-Plugins.git
-git clone https://github.com/DataSploit/datasploit.git
-git clone https://github.com/offensive-security/exploit-database.git
-git clone https://github.com/Exploit-install/snmpwn
-git clone https://github.com/offensive-security/exploit-database
-git clone https://github.com/offensive-security/exploit-database-bin-sploits
-git clone https://github.com/offensive-security/exploit-database-papers
-git clone https://github.com/OWASP/joomscan.git
+downloadGit https://github.com/frizb/OSCP-Survival-Guide.git
+downloadGit https://github.com/codingo/Reconnoitre.git
+downloadGit https://github.com/erik1o6/oscp.git
+downloadGit https://github.com/scipag/vulscan.git
+downloadGit https://github.com/darkoperator/Metasploit-Plugins.git
+downloadGit https://github.com/DataSploit/datasploit.git
+downloadGit https://github.com/offensive-security/exploit-database.git
+downloadGit https://github.com/Exploit-install/snmpwn.git
+downloadGit https://github.com/offensive-security/exploit-database.git
+downloadGit https://github.com/offensive-security/exploit-database-bin-sploits.git
+downloadGit https://github.com/offensive-security/exploit-database-papers.git
+downloadGit https://github.com/OWASP/joomscan.git
+downloadGit https://github.com/JohnTroony/php-webshells
+
+
 
 #wiki Technical content
 cd ..; mkdir wiki; cd wiki
-git clone https://github.com/cappetta/SecDevOps-Toolkit.wiki.git
+downloadGit https://github.com/cappetta/SecDevOps-Toolkit.wiki.git
 
 # Cloud SecDevOps tooling
-dir=Defensive;
+dir=SecDevOps;
 cd ..; mkdir $dir; cd $dir
-git clone https://github.com/Netflix/security_monkey.git
-git clone https://github.com/dowjones/hammer.git
-git clone https://github.com/docker/docker-bench-security.git
+downloadGit https://github.com/Netflix/security_monkey.git
+downloadGit https://github.com/dowjones/hammer.git
+downloadGit https://github.com/docker/docker-bench-security.git
 
-dir=education;cd ..; mkdir $dir; cd $dir
-git clone https://github.com/scottslowe/learning-tools.git
-git clone https://github.com/nicholasjackson/slack-bot-lex-lambda.git
-git clone https://github.com/fedekau/terraform-with-circleci-example.git
+
+dir=DevOps;cd ..; mkdir $dir; cd $dir
+downloadGit https://github.com/scottslowe/learning-tools.git
+downloadGit https://github.com/nicholasjackson/slack-bot-lex-lambda.git
+downloadGit https://github.com/fedekau/terraform-with-circleci-example.git
 
 # Vulnerable AWS configurations
-git clone https://github.com/RhinoSecurityLabs/cloudgoat.git
+downloadGit https://github.com/RhinoSecurityLabs/cloudgoat.git
 
 
