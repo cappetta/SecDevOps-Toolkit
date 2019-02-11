7defbe4b2b10a4db0e878f88ae16617d1df4e519
cappetta <thomas.cappetta@gmail.com>
2019-02-11 01:33:50 -0500

    fixing kali manifest

--- tooling/terraform/kali/kali.tf
+++ tooling/terraform/kali/kali.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "kali" {
   ami = "ami-10b19275"
   instance_type = "${var.instance_type}"
-  subnet_id = "${var.subnet}"
+  subnet_id = "${var.subnet_id}"
   key_name = "${var.key_name}"
   vpc_security_group_ids = ["${var.secgroup_id}"]
   user_data = "${file("${var.linux_userdata}")}"
@@ -20,7 +20,7 @@ resource "aws_instance" "kali" {
   private Subnet
 */
 resource "aws_subnet" "us-east-2a_private" {
-  vpc_id = "${var.vpc_default}"
+  vpc_id = "${var.vpc_id}"
 
   cidr_block = "${var.private_subnet_cidr}"
   availability_zone = "us-east-2a"
